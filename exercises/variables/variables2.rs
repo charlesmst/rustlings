9ca5e4953b192a719f1d13d8d114fdcd2a9f5d6c
Charles Michael Stein <charles.mst@gmail.com>
2023-08-19 09:49:17 -0300

    basic solutions

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,10 +3,11 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let x;
+    //declare a int variable called y
+    let x:i64;
+    x = 10;
     if x == 10 {
         println!("x is ten!");
     } else {
